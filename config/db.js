790fb566c5c4b4013e88319a93d36bed331c286a
Joey Chen <joeyshchen@gmail.com>
2024-08-13 14:04:04 -0700

    .env files

--- config/db.js
+++ config/db.js
@@ -1,16 +1,13 @@
 import { MongoClient } from "mongodb";
-import { connectionStringMongoDB } from "../config.js";
 
-export const connectionStringMongoDB = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@tinyurl.v9gjwpw.mongodb.net/?retryWrites=true&w=majority&appName=tinyUrl`;
-
-const connectionString = connectionStringMongoDB || "";
-const client = new MongoClient(connectionString);
+const connectionStringMongoDB = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@tinyurl.v9gjwpw.mongodb.net/?retryWrites=true&w=majority&appName=tinyUrl`;
+const client = new MongoClient(connectionStringMongoDB);
 let conn;
 
 try {
-        conn = await client.connect();
-} catch(e) {
-        console.error(e);
+	conn = await client.connect();
+} catch (e) {
+	console.error(e);
 }
 let db = conn.db("metadata");
 export default db;
